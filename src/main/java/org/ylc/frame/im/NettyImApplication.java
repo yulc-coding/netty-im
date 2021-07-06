7bf4e33011d7b3f8eec54da65327cc9fdc56d0c9
Yulc <350009335@qq.com>
2021-07-07 00:39:11 +0800

    init

---
+++ src/main/java/org/ylc/frame/im/NettyImApplication.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.im;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettyImApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettyImApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/ylc/frame/im/NettyImApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ylc.frame.im;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettyImApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
